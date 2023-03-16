13cc23dbc90a2c024aa3349a6beba7e5e3774b18
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-16 19:57:13 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
+    static int countYears(double balance, double rate, double withdraw) {
+        if (balance * (rate/100) >= withdraw) {
+            return Integer.MAX_VALUE;
+        } else {
+            int years = 0;
+            while (true) {
+                balance = balance + balance * (rate/100) - withdraw;
+                if (balance < 0) {
+                    break;
+                }
+                years++;
+            }
+            return years;
+        }
+    }
+
     public static void main(String[] args) {
         // Om vi har X kronor, och en ränta på Y%, hur många år kan vi ta ut Z kronor.
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ange summan: ");
+        double balance = input.nextDouble();
+        System.out.print("Ange räntesatsen: ");
+        double rate = input.nextDouble();
+        System.out.print("Ange uttaget: ");
+        double withdraw = input.nextDouble();
 
+        System.out.println("Antal år: " + countYears(balance, rate, withdraw));
     }
 }
